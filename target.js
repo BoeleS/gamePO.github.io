fbd581dd6a85f75cf0d8ef17eaf0202cec123117
BoeleS <117627@vituscollege.nl>
2026-02-09 13:10:48 +0100

    d

--- target.js
+++ target.js
@@ -2,8 +2,8 @@ function createTarget(mode) {
   const size = mode === "grid" ? 24 : 30;
 
   return {
-    x: Math.random() * (canvas.width - 100) + 50,
-    y: Math.random() * (canvas.height - 100) + 50,
+    x: Math.random() * (window.innerWidth - 100) + 50,
+    y: Math.random() * (window.innerHeight - 100) + 50,
     r: size,
     vx: mode === "tracking" ? Math.random() * 4 - 2 : 0,
     vy: mode === "tracking" ? Math.random() * 4 - 2 : 0
